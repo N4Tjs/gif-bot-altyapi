d3297c31770a3ddab83690ea8d299686f88e02d4
baybeku <40503804+baybeku@users.noreply.github.com>
2020-10-28 18:19:39 +0300

    ♻️

---
+++ src/commands/avatar.js
@@ -0,0 +1,20 @@
+const { Discord, MessageEmbed } = require('discord.js');
+const { options, db } = global;
+const moment = require('moment');
+moment.locale('tr');
+require('moment-timezone');
+
+
+exports.run = async (client, message, args) => {
+    const victim = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+    let embed = new MessageEmbed().setAuthor(`${victim.user.tag} • Adlı Üyenin Avatarı` , victim.user.avatarURL({ dynamic:true, size:2048})).setColor(options.bot.embedColor).setFooter(`${message.author.tag} tarafından istendi.`).setTimestamp();
+    embed.setImage(victim.user.avatarURL({ dynamic: true, size: 2048 }))
+    message.channel.send(embed)
+};
+
+exports.config = {
+    commands: ["avatar"],
+    usage: "[p]avatar < Kullanıcı ( opsiyonel ) >",
+    enabled: true,
+    guildOnly: true
+}
